f3b2ba36f3b852d4e510763ed52296c86e324403
Greg <15948327+reflectivecode@users.noreply.github.com>
2020-03-01 23:28:18 -0600

    add WhenAll for Task

--- TaskEx/WhenAll.cs
+++ TaskEx/WhenAll.cs
@@ -5,6 +5,11 @@ namespace ReflectiveCode.TaskEx
 {
     public static partial class TaskEx
     {
+        public static Task WhenAll(this IEnumerable<Task> tasks)
+        {
+            return Task.WhenAll(tasks);
+        }
+
         public static Task<T[]> WhenAll<T>(this IEnumerable<Task<T>> tasks)
         {
             return Task.WhenAll(tasks);
