f4fe9b418886f387b3e8e294781eb0710f25d705
Greg <15948327+reflectivecode@users.noreply.github.com>
2020-03-13 23:11:14 -0500

    add OnSuccess

---
+++ TaskEx/OnSuccess.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ReflectiveCode.TaskEx
+{
+    public static partial class TaskEx
+    {
+        public static async Task<T> OnSuccess<T>(this Task<T> task, Action onSuccess)
+        {
+            var result = await task;
+            onSuccess();
+            return result;
+        }
+
+        public static async ValueTask<T> OnSuccess<T>(this ValueTask<T> task, Action onSuccess)
+        {
+            var result = await task;
+            onSuccess();
+            return result;
+        }
+
+        public static async Task<T> OnSuccess<T>(this Task<T> task, Action<T> onSuccess)
+        {
+            var result = await task;
+            onSuccess(result);
+            return result;
+        }
+
+        public static async ValueTask<T> OnSuccess<T>(this ValueTask<T> task, Action<T> onSuccess)
+        {
+            var result = await task;
+            onSuccess(result);
+            return result;
+        }
+
+        public static async Task<T> OnSuccess<T>(this Task<T> task, Action<T, Task<T>> onSuccess)
+        {
+            var result = await task;
+            onSuccess(result, task);
+            return result;
+        }
+
+        public static async ValueTask<T> OnSuccess<T>(this Task<T> task, Action<T, ValueTask<T>> onSuccess)
+        {
+            var result = await task;
+            onSuccess(result, new ValueTask<T>(result));
+            return result;
+        }
+
+        public static async Task<T> OnSuccessAwait<T>(this Task<T> task, Func<Task> onSuccess)
+        {
+            var result = await task;
+            await onSuccess();
+            return result;
+        }
+
+        public static async ValueTask<T> OnSuccessAwait<T>(this ValueTask<T> task, Func<ValueTask> onSuccess)
+        {
+            var result = await task;
+            await onSuccess();
+            return result;
+        }
+
+        public static async Task<T> OnSuccessAwait<T>(this Task<T> task, Func<T, Task> onSuccess)
+        {
+            var result = await task;
+            await onSuccess(result);
+            return result;
+        }
+
+        public static async ValueTask<T> OnSuccessAwait<T>(this ValueTask<T> task, Func<T, ValueTask> onSuccess)
+        {
+            var result = await task;
+            await onSuccess(result);
+            return result;
+        }
+
+        public static async Task<T> OnSuccessAwait<T>(this Task<T> task, Func<T, Task<T>, Task> onSuccess)
+        {
+            var result = await task;
+            await onSuccess(result, task);
+            return result;
+        }
+
+        public static async ValueTask<T> OnSuccessAwait<T>(this ValueTask<T> task, Func<T, ValueTask<T>, ValueTask> onSuccess)
+        {
+            var result = await task;
+            await onSuccess(result, new ValueTask<T>(result));
+            return result;
+        }
+    }
+}
